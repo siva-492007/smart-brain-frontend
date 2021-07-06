aedfd2e270aa556613534412e4086935cd81fb4d
siva-007 <sivavignesh761@gmail,com>
2021-07-06 23:27:26 +0530

    frontend

---
+++ src/Link.js
@@ -0,0 +1,7 @@
+// export const backendURL = 'http://localhost:3000/';
+export const backendURL = 'https://dry-brushlands-51181.herokuapp.com/';
+export const registerURL = backendURL + 'register';
+export const signInURL = backendURL + 'signin';
+export const imageURl = backendURL + 'imageurl';
+export const updateURL = backendURL + 'update-image';
+export const profileURL = backendURL + 'profile/:id';
